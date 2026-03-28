3fc03126b6d8e77f6aa299fd357775b0998fa327
KJM kjm <ST10283668@vcconnect.edu.za>
2026-03-28 15:43:53 +0200

    Created project and added automatic properties

---
+++ ChatBot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Securitybot
+{
+    internal class ChatBot
+    {
+        public string UserName { get; set; }
+        public string Botname { get; set; }
+        public string Greeting { get; set; }
+    }
+}
