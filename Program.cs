e501c746f7c168073a2d56f9dd7f7c5255ed9005
KJM kjm <ST10283668@vcconnect.edu.za>
2026-03-29 14:34:16 +0200

    I have add the user name input and a greeting with their name after

--- Program.cs
+++ Program.cs
@@ -5,6 +5,18 @@
         static void Main(string[] args)
         {
             Display.WelcomeMessage();
+            
+           ChatBot bot = new ChatBot();
+           bot.Botname = "SecurityBot";
+
+
+            Console.WriteLine("What is your name?");
+            bot.UserName = Console.ReadLine();
+
+
+            bot.Greeting = "Hello ,"+bot.UserName+"  I am "+bot.Botname+", here to  help with Cyber Security needs ";
+            Console.WriteLine(bot.Greeting);    
+
         }
     }
 }
